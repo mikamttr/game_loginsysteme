a6750b7b3b6e1d07227d3406e212fc72c935d0bc
mikamttr <mika348@live.fr>
2023-09-02 00:13:16 +0200

    v0.55

--- app/views/profile.php
+++ app/views/profile.php
@@ -3,10 +3,48 @@
     Back to Home
 </a>
 
-
-<form action="">
-    <div>Full Name : <?php echo $_SESSION['usersName']; ?></div>
-    <div>Your best score : 89</div>
-    <div>Username</div>
-    <div>email</div>
+<p class="fs-3">Profile Settings</p>
+<p class="fs-5"><?php echo $_SESSION['usersName'] ?></p>
+<form>
+    <div class="row mt-2">
+        <div class="col-lg-6 col-md-8">
+            <label class="form-label">Email</label>
+            <input type="email" class="form-control" value="<?php echo $_SESSION['usersEmail']; ?>">
+        </div>
+    </div>
+    <div class="row mt-2">
+        <div class="col-lg-6 col-md-8">
+            <label class="form-label">Username</label>
+            <input type="text" class="form-control" value="<?php echo $_SESSION['usersUid'] ?>">
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col-lg-6 col-md-8">
+            <label for="currentPassword" class="form-label">Current Password</label>
+            <input type="password" id="currentPassword" class="form-control" placeholder="Enter your current password">
+        </div>
+    </div>
+    <div class="row mt-2">
+        <div class="col-lg-6 col-md-8">
+            <label for="newPassword" class="form-label">New Password</label>
+            <input type="password" id="newPassword" class="form-control" placeholder="Enter new password" aria-describedby="passwordHelpBlock">
+            <div id="passwordHelpBlock" class="form-text">
+                <span class="text-danger">*</span>
+                Your password must be at least 6 characters long
+            </div>
+        </div>
+    </div>
+    <div class="row mt-2">
+        <div class="col-lg-6 col-md-8">
+            <label for="repeatPassword" class="form-label">Repeat Password</label>
+            <input type="password" id="repeatPassword" class="form-control" placeholder="Repeat new password">
+        </div>
+    </div>
+    <div class="row my-4">
+        <div class="col-lg-6 col-md-8 mt-2">
+            <div class="d-grid gap-3 d-md-flex justify-content-end">
+                <input type="submit" class="btn btn-dark px-5" value="Submit">
+            </div>
+        </div>
+    </div>
 </form>
