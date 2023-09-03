5c6fc3816d0cadfdaf046772d4e89440006b5650
Mika <117665372+mikamttr@users.noreply.github.com>
2023-09-03 17:56:20 +0200

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'app/helpers/helper.php';
-
-session_start();
-unset($_SESSION['usersId']);
-unset($_SESSION['usersName']);
-unset($_SESSION['usersEmail']);
-session_destroy();
-redirect("index.php");
